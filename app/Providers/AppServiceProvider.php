5eb0d6649c29dd452f7f60f9c35561e2d8793f0a
SalahDerbas <salahderbas123@gmail.com>
2022-08-23 19:42:03 +0300

    updat23232323

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if($this->app->environment('production')) {
-            \URL::forceScheme('https');
-        }
+        // if($this->app->environment('production')) {
+        //     \URL::forceScheme('https');
+        // }
     }
 }
